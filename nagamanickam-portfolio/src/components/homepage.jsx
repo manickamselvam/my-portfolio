2983cdf428980576dfec2a0279202e69d87ceeac
Nagamanickam <nagamanickam@konnectify.co>
2024-08-20 23:14:07 +0530

    background banner developed

---
+++ nagamanickam-portfolio/src/components/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './homepage.css'
+
+const homepage = () => {
+    return (
+        <>
+        <section className='home_container'>
+            <p>Home Container</p>
+        </section>
+        </>
+    )
+}
+
+export default homepage
