2cad3277f0e6afd770fe7a6a8852da09ff2beb61
Nagamanickam <nagamanickam@konnectify.co>
2024-12-23 00:13:46 +0530

    completed full structure, animation and content change is pending

--- nagamanickam-portfolio/src/components/Navbar.jsx
+++ nagamanickam-portfolio/src/components/Navbar.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import logo from "../assets/NAG-artist-Logo-New.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = ({ contactSection }) => {
+  const scrollToContact = () => {
+    if (contactSection.current) {
+      contactSection.current.scrollIntoView({
+        behavior: "smooth", // Smooth scroll
+        block: "start", // Align to the top of the viewport
+      });
+    }
+  };
   return (
-    <nav className=" mb-20 flex items-center justify-between py-6  ">
+    <nav className="sticky top-10 z-10 flex items-center justify-between mx-10 pb-20 mt-10 text-neutral-300">
       <div>
         <img src={logo} alt="logo" className="h-14 w-14" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaGithub />
-        <FaInstagram />
+        <FaPhoneAlt onClick={scrollToContact} />
       </div>
     </nav>
   );
